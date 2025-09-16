52468b45b9036b8c993fd0cc61c209c60eb5689d
Javan9790 <232107521+Javan9790@users.noreply.github.com>
2025-09-16 16:48:49 +0300

    Update height,id and bankbalance.c

--- introduction to c/height,id and bankbalance.c
+++ introduction to c/height,id and bankbalance.c
@@ -4,20 +4,24 @@ Name:Javan Ichingwa
 Reg:PA106/28767/G/25
 Description:Program to display my height,ID number and Bank balance
 */
+//import the library standard input output header
 #include<stdio.h>//preprocessor directive scanf(),printf()
 
 //main function
 int (){
+//working with height: input and output height
     int height;
     printf("Enter your height:");
     scanf("%d"&height);
     printf("Your height is %d/n",height); 
 
+//working with id number: input and output id number
     int IDnumber;
     printf("Enter your IDnumber:");
     scanf("%d"&IDnumber);
     printf("Your IDnumber is %d/n,"IDnumber);
 
+//working with bank balance: input and output bank balance
     int bankbalance;
     printf("Enter your bankbalance:");
     scanf("%d"&bankbalance);
@@ -25,4 +29,5 @@ int (){
 
     return 0;
 }
+
 
