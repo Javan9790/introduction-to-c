46958b5573a6cdffbe9c429dd3c5d9f02e57dacc
Javan9790 <232107521+Javan9790@users.noreply.github.com>
2025-09-16 16:43:03 +0300

    Add files via upload

---
+++ introduction to c/height,id and bankbalance.c
@@ -0,0 +1,28 @@
+// Good day 
+/* 
+Name:Javan Ichingwa
+Reg:PA106/28767/G/25
+Description:Program to display my height,ID number and Bank balance
+*/
+#include<stdio.h>//preprocessor directive scanf(),printf()
+
+//main function
+int (){
+    int height;
+    printf("Enter your height:");
+    scanf("%d"&height);
+    printf("Your height is %d/n",height); 
+
+    int IDnumber;
+    printf("Enter your IDnumber:");
+    scanf("%d"&IDnumber);
+    printf("Your IDnumber is %d/n,"IDnumber);
+
+    int bankbalance;
+    printf("Enter your bankbalance:");
+    scanf("%d"&bankbalance);
+    printf("Your bankbalance is %d/n"bankbalance);
+
+    return 0;
+}
+
---
+++ introduction to c/units_uits.c
@@ -0,0 +1,20 @@
+//Good day 
+/*
+Name:Javan Ichingwa
+Reg:PA106/28767/25
+Description:Program to dispay all units for this semester
+*/
+#include<stdio.h>//preprocessor directive :scanf(),
+
+//main function
+int main (){
+    printf("SPC 2101 Introduction to programming and problem solving\n");
+    printf("SPI 2102 Computer System and Organisation\n");
+    printf("SPM 2100 Descrete Mathematics\n");
+    printf("SPM 2104 Mathematics for Science\n");
+    printf("SSE 2103 Funntament of System Analysis and Design\n");
+    printf("UCU 2105 Funtamentals of ICT\n");
+    printf("UCU 2106 Communication and Information Literaly Skills\n");
+
+    return 0;
+}
