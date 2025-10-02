dc46c59950d6eadd69836d77beeb339ae36f95e2
Javan9790 <232107521+Javan9790@users.noreply.github.com>
2025-10-02 17:39:11 +0300

    Add files via upload

---
+++ Eligibility.c
@@ -0,0 +1,30 @@
+/*
+A student is eligible for exam if:
+Attendance is >=75% ,ADD
+Average marks are >=40
+Otherwise print"Not eligible"
+*/
+
+# include <stdio.h>
+//main function
+int main(){
+	//variable declaration
+float attendance,average_marks;
+
+//prompt the user to enter attendance
+printf(" Enter attendance\t:" );
+scanf("%f", &attendance);
+
+//prompt the user to enter average-marks
+printf(" Enter average_marks\t:");
+scanf("%f", &average_marks);
+
+
+if(attendance >=75 & average_marks >=40\t){
+printf("eligible");
+}
+else{
+	printf("Not eligible");
+}
+return 0;
+}
