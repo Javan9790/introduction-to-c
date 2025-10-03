e97a9b7fc544a65b2e0f401acadebdee2590d74b
Javan9790 <232107521+Javan9790@users.noreply.github.com>
2025-10-03 11:46:02 +0300

    Add files via upload

---
+++ Mobile_databundle.c
@@ -0,0 +1,38 @@
+/*
+Mobile Data Bundle purchase
+100 MB for 50 KES
+500 MB for 200 KES
+1 GB for350 KES
+2 GB for 600 KES
+*/
+
+# include<stdio.h>
+
+int main(){
+	//variable declaration
+	float bundles , cost, option;
+	
+	//prompt the user to enter bundles
+	printf("Enter bundles .\n");
+	scanf("%f", &bundles);
+	
+	switch()
+	{
+		option 1;100 MB
+		cost; 50
+	
+	option 2;500 MB
+	cost;200
+
+option 3;1 GB
+cost;350
+
+option 4;2 GB
+cost;600 
+
+default;
+
+
+		
+	}
+}
