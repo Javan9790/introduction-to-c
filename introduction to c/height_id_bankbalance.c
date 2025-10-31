049058f930ad47b8c727bc7e94cf25dfb00a7d26
Javan9790 <232107521+Javan9790@users.noreply.github.com>
2025-10-31 09:38:15 +0300

    Update height_id_bankbalance.c

--- introduction to c/height_id_bankbalance.c
+++ introduction to c/height_id_bankbalance.c
@@ -1,4 +1,4 @@
-// Good day 
+ 
 /* 
 Name:Javan Ichingwa
 Reg:PA106/28767/G/25
@@ -12,22 +12,23 @@ int (){
 //working with height: input and output height
     int height;
     printf("Enter your height:");
-    scanf("%d"&height);
+    scanf("%d",&height);
     printf("Your height is %d/n",height); 
 
 //working with id number: input and output id number
     int IDnumber;
     printf("Enter your IDnumber:");
-    scanf("%d"&IDnumber);
-    printf("Your IDnumber is %d/n,"IDnumber);
+    scanf("%d",&IDnumber);
+    printf("Your IDnumber is %d/n",IDnumber);
 
 //working with bank balance: input and output bank balance
     int bankbalance;
     printf("Enter your bankbalance:");
-    scanf("%d"&bankbalance);
-    printf("Your bankbalance is %d/n"bankbalance);
+    scanf("%d",&bankbalance);
+    printf("Your bankbalance is %d/n",bankbalance);
 
     return 0;
 }
+
 
 
