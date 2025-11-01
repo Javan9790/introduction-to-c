bde9f5ef162062ddd9f432787de059e070aa5844
Javan9790 <232107521+Javan9790@users.noreply.github.com>
2025-11-01 10:07:22 +0300

    Update Data_purchase.c

--- introduction to c/Data_purchase.c
+++ introduction to c/Data_purchase.c
@@ -9,18 +9,18 @@ option 1,   100MB  bundles =50 KES cost
 
 int main (){
 	//variable declaration
-	int choice;
+	int option;
 	
 	//prompt the user to enter their choice
 	printf("Enter an option:\n" );
-	printf("option 1\n");
-	printf("option 2\n");
-	printf("option 3\n");
-	printf("0ption 4\n");
+	printf("option 1 is 100MB kes50\n");
+	printf("option 2 is 500MB kes200\n");
+	printf("option 3 is 1GB kes350\n");
+	printf("0ption 4 is 2GB kes600\n");
 	
-	if(choice >=1 && choice <=4)
+	if(option >=1 && option <=4)
 		
-		switch(choice)
+		switch(option)
 		{
 			case 1:
 				printf("you typed 100 1\n");
@@ -44,4 +44,5 @@ int main (){
 	
 		
 		
+
 }
